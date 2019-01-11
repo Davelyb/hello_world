5bce4edaef5152b9070ef03b6eedb0c59b9707cd
yabo.liu@equotaenergy.com <yabo.liu@equotaenergy.com>
2019-01-11 15:02:57 +0800

    yay

--- mySpider/mySpider/spiders/new_weather.py
+++ mySpider/mySpider/spiders/new_weather.py
@@ -11,11 +11,11 @@ class MySpider(Spider):
 
     def __init__(self):
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('window-size=1920x3000')  # 指定浏览器分辨率
-        chrome_options.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
-        chrome_options.add_argument('--hide-scrollbars')  # 隐藏滚动条, 应对一些特殊页面
-        chrome_options.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片, 提升速度
-        chrome_options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
+        chrome_options.add_argument('window-size=1920x3000')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--hide-scrollbars')
+        chrome_options.add_argument('blink-settings=imagesEnabled=false')
+        chrome_options.add_argument('--headless')
         self.browser = webdriver.Chrome(chrome_options=chrome_options, executable_path='/usr/local/Caskroom/chromedriver/2.45/chromedriver')
         # self.browser.set_page_load_timeout(30)
 
@@ -48,8 +48,6 @@ class MySpider(Spider):
                 item['wind_direct'] = wind_direct[0]
                 item['wind_level'] = float(wind_level[0].strip('级'))
 
-                # print('实时温度:', current_tem)
-                # print('相对湿度:', humidity)
             elif domain == 'weather':
                 seven_day = response.xpath("//div[@id='7d']/ul[@class='t clearfix']")
                 seven_day_lt = []
